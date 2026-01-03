e4280ebebd9ba2236aafd8971c0cf084a2365ef5
shkelqim dedushi <shkelqimdedushi@gmail.com>
2026-01-03 18:40:01 +0100

    pjesa e 2t

---
+++ pjesa2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+ void Ora::shtoSekond(){
+
+    sekonda++;
+
+    if(sekonda == 60){
+
+        sekonda = 0;
+
+        minuta++;
+
+        if ( minuta == 60){
+
+            minuta=0;
+
+            ora++;
+
+        }
+
+    }
+
+    else if(minuta == 60){
+
+        minuta = 0;
+
+        ora++;
+
+    }
+
+    else if(ora == 24 ){
+
+        ora=0;
+
+    }
+
+
+   void Ora::shtoMinute(){
+
+    minuta++;
+
+    if ( minuta == 60){
+
+            minuta=0;
+
+            ora++;
+
+        }
+
+    else if( ora == 24 ){
+
+        ora=0;
+
+    }
+
+    
+
+    return 0;
+}
